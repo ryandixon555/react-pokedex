3f4495d58ccd61d4ee771f6bda94c10b36dded5e
rdixon555 <ryan555@sky.com>
2020-04-10 13:06:52 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component } from "react"
-import "./App.css"
 
+import './App.css';
 
+import backgroundImage from './pattern.png';
 
-import NavBar from './components/layout/NavBar'
+import NavBar from './components/layout/NavBar';
+import Dashboard from './components/layout/Dashboard';
+import SearchBar from './components/search/SearchBar';
+import Pokemon from './components/pokemon/Pokemon';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar/>>
-      </div>
-    )
+      <Router>
+        <div className="App" style={{ background: `url(${backgroundImage})` }}>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
-export default App
+export default App;
